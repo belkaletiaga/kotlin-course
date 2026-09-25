ea924df79ec793ae3fe4cb607a6bce7347a72c09
User Lyust <anastasiya.belonozhko@gmail.com>
2026-09-25 22:22:24 +0700

    add task 14 15

--- src/main/kotlin/org/example/lessons/lesson07/homeworks/Cycle.kt
+++ src/main/kotlin/org/example/lessons/lesson07/homeworks/Cycle.kt
@@ -25,6 +25,7 @@ fun main() {
     }
     println()
 
+    println("#4")
     //4. Создайте цикл for, который выводит числа от 10 до 1, уменьшая их на 2.
     for (i in 10 downTo 1) {
         print(i - 2)
@@ -32,6 +33,7 @@ fun main() {
     }
     println()
 
+    println("#5")
     //5. Используйте цикл for с шагом 2 для вывода чисел от 1 до 9.
     for (i in 1..9 step 2) {
         print(i)
@@ -39,6 +41,7 @@ fun main() {
     }
     println()
 
+    println("#6")
     //6. Напишите цикл for, который выводит каждое третье число в диапазоне от 1 до 20.
     for (i in 1..20) {
         if (i % 3 == 0) {
@@ -48,23 +51,26 @@ fun main() {
     }
     println()
 
+    println("#7")
     // 7. Создайте числовую переменную 'size'. Используйте цикл for с шагом 2 для вывода чисел от 3 до size
     // не включая size.
     var size = 15
-    for (i in 3..size step 2) {
+    for (i in 3 until size step 2) {
         print(i)
         print(" ")
     }
     println()
 
+    println("#8")
     //8. Создайте цикл while, который выводит квадраты чисел от 1 до 5.
     var counter = 0
     while (counter++ < 5) {
-        print(counter)
+        print(counter * counter)
         print(" ")
     }
     println()
 
+    println("#9")
     //9. Напишите цикл while, который уменьшает число от 10 до 5. После этого вывести результат в консоль
     var counter1 = 11
     while (counter1-- > 5) {
@@ -73,6 +79,7 @@ fun main() {
     }
     println()
 
+    println("#10")
     //10. Используйте цикл do while, чтобы вывести числа от 5 до 1.
     var counter2 = 5
     do {
@@ -81,6 +88,7 @@ fun main() {
     } while (counter2-- > 1)
     println()
 
+    println("#11")
     //11. Создайте цикл do while, который повторяется, пока счетчик меньше 10, начиная с 5.
     var counter3 = 5
     do {
@@ -89,7 +97,32 @@ fun main() {
     } while (++counter3 < 10)
     println()
 
-    //11. В цикле for от 1 до 10 используйте continue, чтобы пропустить четные числа.
+    println("#12")
+    //12. Напишите цикл for от 1 до 10 и используйте break, чтобы выйти из цикла при достижении 6.
+    for (i in 1 .. 10){
+        if (i == 6) {
+            break
+        }
+        print(i)
+        print(" ")
+    }
+    println()
+
+    println("#13")
+    //13. Создайте цикл while, который бесконечно выводит числа, начиная с 1, но прерывается при достижении 10.
+    var counter4 = 1
+    while (true){
+        if (counter4 == 10) {
+            break
+        }
+        print(counter4)
+        counter4++
+        print(" ")
+    }
+    println()
+
+    println("#14")
+    //14. В цикле for от 1 до 10 используйте continue, чтобы пропустить четные числа.
     for (i in 1..10) {
         if (i % 2 == 0) {
             continue
@@ -99,13 +132,14 @@ fun main() {
     }
     println()
 
-    //12. Напишите цикл while, который выводит числа от 1 до 10, но пропускает числа, кратные 3.
-    var counter4 = 0
-    while (counter4++ < 10) {
-        if (counter4 % 3 == 0) {
+    println("#15")
+    //15. Напишите цикл while, который выводит числа от 1 до 10, но пропускает числа, кратные 3.
+    var counter5 = 0
+    while (counter5++ < 10) {
+        if (counter5 % 3 == 0) {
             continue
         }
-        print(counter4)
+        print(counter5)
         print(" ")
     }
 
